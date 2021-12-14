b2b6fee2989f081b387cbadfc6e5223fe5cf1a88
flavioflnlima <flavioflnlima@gmail.com>
2021-12-13 23:54:51 -0300

    Add project files.

---
+++ RabbitPublish/Controllers/MessageController.cs
@@ -0,0 +1,57 @@
+﻿using Messages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitPublish.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly ConnectionFactory _factory;
+        private readonly RabbitMqConfiguration _config;
+
+        public MessageController(IOptions<RabbitMqConfiguration> options)
+        {
+            _config = options.Value;
+
+            _factory = new ConnectionFactory
+            {
+                HostName = _config.Host
+            };
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] MessageRabbit message)
+        {
+            using (var connection = _factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: _config.Queue,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                        );
+
+                    var messageSerialized = JsonConvert.SerializeObject(message);
+                    var messageBytes = Encoding.UTF8.GetBytes(messageSerialized);
+
+                    channel.BasicPublish(
+                        exchange:"",
+                        routingKey: _config.Queue,
+                        basicProperties: null,
+                        body: messageBytes
+                        );
+                }
+            }
+            return Accepted();
+        }
+    }
+}
